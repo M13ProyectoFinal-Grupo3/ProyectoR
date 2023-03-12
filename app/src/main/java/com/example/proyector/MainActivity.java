054217edad6306459ef0ed0f70505759633bb186
Nacho <pueblafarinaignacio@gmail.com>
2023-03-12 22:11:35 +0000

    Revert "Prueba de Commit and Push"
    This reverts commit efecf6cf5ad5bc891486365c007edb92bcd0ad7e.

--- app/src/main/java/com/example/proyector/MainActivity.java
+++ app/src/main/java/com/example/proyector/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-//Comentario de prueba
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
