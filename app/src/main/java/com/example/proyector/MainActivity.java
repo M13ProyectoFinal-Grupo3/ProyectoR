f214aa79ae937443b9b927cb3e3db44e3815c144
naxval <116650174+naxval@users.noreply.github.com>
2023-03-17 18:04:27 +0100

    qr2
    (cherry picked from commit 9417489d3a0484ff3833ff0a708cd1df584d7f35)

--- app/src/main/java/com/example/proyector/MainActivity.java
+++ app/src/main/java/com/example/proyector/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.proyector;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnForm = (Button) findViewById(R.id.btnNuevaCuenta);
         Button btnAccess = (Button) findViewById(R.id.btnAcceso);
+        Button btnLeerQR = (Button) findViewById(R.id.btnLeerQR);
+        Button btnGenQR = (Button) findViewById(R.id.btnGenQR);
+        imageView= (ImageView) findViewById(R.id.imageQR);
 
         btnForm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,11 +54,68 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnLeerQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                escanear();
+            }
+        });
+
+        btnGenQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageBitmap(generateQRCodeImage("12324567890"));
+            }
+        });
+
+    }
+
+    // escanear qr
+    public void escanear(){
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+        options.setPrompt("ESCANEAR QR");
+        options.setCameraId(0);
+        options.setOrientationLocked(false);
+        options.setBeepEnabled(true);
+        options.setCaptureActivity(CapActivity.class);
+        options.setBarcodeImageEnabled(false);
+        barcodeLauncher.launch(options);
+
+    }
+
+    // contrato leer qr
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),result->{
+        if (result.getContents() != null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }});
+
+    // genera codigo QR
+    private Bitmap generateQRCodeImage(String text) {
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        Bitmap bmp = null;
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,500,500);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            bmp = barcodeEncoder.createBitmap(bitMatrix);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return bmp;
     }
 }
-
 /*
 // Create a new user with a first and last name
+
 Map<String, Object> user = new HashMap<>();
 user.put("first", "Ada");
 user.put("last", "Lovelace");
